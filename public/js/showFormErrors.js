bab8ecdbaa087d82c6f36172fd064d8647e8ae3b
Alexander Kyle Chua <kyle.chua16@gmail.com>
2018-06-25 14:23:03 -0700

    removed old JS files

--- public/js/showFormErrors.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var errorMsgs = document.getElementsByClassName("invalid-feedback");
-    if (typeof errorMsgs !== 'undefined') {
-        for (var i=0; i<errorMsgs.length; i++) {
-        errorMsgs[i].style.display = "block";
-        }
-    }
-});
